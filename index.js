d70fb70b9bfbf50e75f96501ad1a6f48fa2a636e
Hung Nguyen <60608285+hungnq99@users.noreply.github.com>
2020-12-10 20:06:30 +0700

    Morgan

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const morgan = require('morgan');
+app.use(morgan('combined'));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
